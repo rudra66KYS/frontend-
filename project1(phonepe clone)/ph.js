b514e91402fcf6cea5a9aaaa1ebde170eea1fcc8
Rudranil Das <137057520+rudra66KYS@users.noreply.github.com>
2025-06-28 17:46:20 +0530

    Add files via upload

---
+++ project1(phonepe clone)/ph.js
@@ -0,0 +1,8 @@
+$('.popup-youtube, .popup-text').magnificPopup({
+    disableOn: 320,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: true
+});
